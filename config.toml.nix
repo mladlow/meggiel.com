2f56ba73552d32b560e971583084fb13705875a2
Meggie Ladlow <meggie@unitedincome.com>
2017-10-21 12:07:52 -0400

    Initial commit

---
+++ config.toml.nix
@@ -0,0 +1,13 @@
+baseURL = "http://example.org/"
+languageCode = "en-us"
+title = "My New Hugo Site"
+theme = "hugo-theme-nix"
+[params]
+  GithubID = "mladlow"
+  TwitterID = "meggiel"
+  LinkedInID = "mladlow"
+  Name = "Meggie Ladlow"
+  Email = "m.ladlow@gmail.com"
+  About = "Hi I'm Meggie. I'm a software engineer. I'm always interested in creating and learning \
+    new things. Reach out!"
+  ProfilePicture = "/content/img/headshot.jpg"
---
+++ content/posts/initial.md
@@ -0,0 +1,6 @@
+---
+title: "Initial"
+date: 2017-10-21T11:49:36-04:00
+draft: true
+---
+
