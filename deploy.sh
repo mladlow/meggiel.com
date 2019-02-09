e0f4df6e2bd56a7e61e2e5fcd76407362b0979fc
Meggie Ladlow <meggie@unitedincome.com>
2019-02-09 18:05:59 -0500

    CSS fix

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,15 @@
 
 . .env
 
+if [ -d public ]
+then
+  echo "Warning: public dir already exists."
+  exit 1
+fi
+
 hugo
-AWS_PROFILE=unimeg_meggie aws s3 sync public/ s3://$MEGGIEL_BUCKET_NAME
+
+#AWS_PROFILE=unimeg_meggie aws s3 sync s3://$MEGGIEL_BUCKET_NAME s3://$MEGGIEL_BACKUP_BUCKET_NAME --delete
+#AWS_PROFILE=unimeg_meggie aws s3 sync public/ s3://$MEGGIEL_BUCKET_NAME --delete
+
+rm -rf public/
