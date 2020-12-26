9d1fc3d2d495ae3c6a2c80d3cc8cd0906f9d1717
Meggie Ladlow <m.ladlow@gmail.com>
2020-12-26 18:29:07 -0500

    Added cp example to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,6 @@ hugo
 
 #AWS_PROFILE=unimeg_meggie aws s3 sync s3://$MEGGIEL_BUCKET_NAME s3://$MEGGIEL_BACKUP_BUCKET_NAME --delete
 #AWS_PROFILE=unimeg_meggie aws s3 sync public/ s3://$MEGGIEL_BUCKET_NAME --delete
+#aws s3 cp --recursive public/ s3://<bucket name>
 
 rm -rf public/
